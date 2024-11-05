675bfbd32756bf966742192016ae226b4ba4403f
DayCris1 <dayene_crist@hotmail.com>
2024-11-04 21:58:06 -0300

    aulas5
    botoes

---
+++ UC2/aula 5/exemplo2/ex2.js
@@ -0,0 +1,26 @@
+document.write("olá terra")
+//alert('ola')
+console.log('ola, tudo bem?')
+var variavel1 = 'olá mundo'
+//alert(variavel1)
+var variavel2 = 123
+//alert(variavel2)
+
+var my_var
+my_var = 'olá, pessoal';
+//alert(my_var)
+my_var = 5;
+//alert(my_var)
+my_var = [1, 10, 'Olá', true];
+//alert(my_var)
+console.log(variavel2)
+//alert(variavel3);
+
+/*Comentario
+em Javascript
+em múltiplas linhas
+*/
+
+document.write("olá marte")
+document.write('<h1>Eu sou um título</h1>')
+document.write('<title> titulo da pagina </title>')
